9424c436d2699e9f93dddef140dab257c0facd5b
Andrew Secco <asecco99@gmail.com>
2021-06-15 21:47:03 -0400

    Geolocation API

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
+import requests
+import socket
 import sys
 
 class Window(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Anonymous Profile Viewer")
+        self.setWindowTitle("IP Location Lookup")
         self.setGeometry(670, 350, 600, 300)
         self.setWindowIcon(QIcon('icon.png'))
 
@@ -23,21 +25,30 @@ class Window(QMainWindow):
         file_menu.addAction("About", self.about_button)
         file_menu.addAction("Exit", self.exit_button)
 
-        self.button = QPushButton("Run", self)
-        self.button.move(230, 40)
+        self.textbox = QLineEdit(self)
+        self.textbox.move(90, 30)
+        self.textbox.resize(400, 35)
+
+        self.button = QPushButton("Lookup", self)
+        self.button.move(230, 75)
         self.button.resize(125, 40)
         self.button.clicked.connect(self.button_click)
 
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(("8.8.8.8", 80))
+        print(s.getsockname()[0])
+        s.close()
 
     def button_click(self):
-        self.show()
+        request = requests.get("http://ip-api.com/json/24.48.0.1").json()
+        print(request)
 
-    def exit_button(self):
-        sys.exit()
 
     def about_button(self):
         QMessageBox.about(self, "About", "Created By: Andrew Secco\n\nhttps://github.com/asecco")
 
+    def exit_button(self):
+        sys.exit()
 
 app = QApplication(sys.argv)
 window = Window()
