ea431e926f87a6545c6bb5f9d7744f68b24de929
Andrew Secco <asecco99@gmail.com>
2021-06-16 00:24:38 -0400

    Full functionality update

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ class Window(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("IP Location Lookup")
-        self.setGeometry(670, 350, 600, 300)
+        self.setFixedSize(600, 300)
         self.setWindowIcon(QIcon('icon.png'))
 
         p = self.palette()
@@ -25,19 +25,29 @@ class Window(QMainWindow):
         file_menu.addAction("Exit", self.exit_button)
 
         self.textbox = QLineEdit(self)
-
-        self.textbox.move(90, 30)
+        self.textbox.move(100, 30)
         self.textbox.resize(400, 35)
 
         self.button = QPushButton("Lookup", self)
-        self.button.move(230, 75)
+        self.button.move(240, 75)
         self.button.resize(125, 40)
         self.button.clicked.connect(self.button_click)
+
+        self.label = QLabel(self)
+        self.label.setWordWrap(True)
+        self.label.move(100, 135)
+        self.label.resize(400, 150)
+
+        self.shortcut = QShortcut(QKeySequence("RETURN"), self)
+        self.shortcut.activated.connect(self.enter_shortcut)
 
     def button_click(self):
         textbox_value = self.textbox.text()
         request = requests.get("http://ip-api.com/json/" + textbox_value).json()
-        print(request)
+        self.label.setText(str(request))
+
+    def enter_shortcut(self):
+        self.button.click()
 
     def about_button(self):
         QMessageBox.about(self, "About", "Created By: Andrew Secco\n\nhttps://github.com/asecco")
